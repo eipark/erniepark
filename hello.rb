ad76a6cccaaf7c44aeca7503c18b0a93539ba517
Ernie <ernestipark@gmail.com>
2012-05-14 13:11:01 -0400

    add xboard

--- hello.rb
+++ hello.rb
@@ -6,3 +6,10 @@ get '/' do
 #	redirect '/index.html'
 end
 
+get '/xboard/' do
+ File.read(File.join('public', 'xboard/index.html'))
+end
+
+get '/xboard/embed.html' do
+ File.read(File.join('public', 'xboard/embed.html'))
+end
